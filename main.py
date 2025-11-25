ea08b19dd7bbcbe1c8ea71918754beeac5cbb9a1
Detzio <130151626+Detzio@users.noreply.github.com>
2025-11-25 20:01:27 +0100

    init

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+
+from snake import Snake
+snake = Snake(400, 300, 20)
+snake.body = [(400, 300), (380, 300), (360, 300)]
+
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Snake Game by Dusséaux Thomas")
+running = True
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0,0,0))
+    snake.draw(screen)
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
---
+++ snake.py
@@ -0,0 +1,28 @@
+import pygame
+
+class Snake:
+    def __init__(self, x, y, body_segment_size):
+        self.body = [(x, y)]
+        self.segment_size = body_segment_size
+        self.direction = (1,0)
+    
+    def move(self, direction):
+        head_x, head_y = self.body[0]
+        dir_x, dir_y = direction
+        new_head = ( head_x + dir_x, head_y + dir_y )
+        self.body.insert(0, new_head)
+        self.body.pop()
+
+    def grow(self):
+        self.body.append(self.body[-1])
+
+    def draw(self, screen):
+        for segment in self.body:
+            pygame.draw.rect(screen, (0,255,0), (segment[0], segment[1], self.segment_size, self.segment_size))
+
+    def check_collision(self, screen_width, screen_height):
+        pass
+
+    def get_head_position(self):
+        return self.body[0]
+    
